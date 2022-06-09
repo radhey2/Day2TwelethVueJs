7c852e2ae5e708e6e79a25363a720aa203410034
lenovo <radheshyamb9@gmail.com>
2022-06-09 11:45:41 +0530

    ADDED : V-HTML Great for strings that have html elements that need to be rendered!

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+ <div id="app">
+  <h3>What is your favorite kind of taco?</h3>
+  <p v-html="tacos"></p>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      tacos: `I like <a href="http://www.epicurious.com/recipes/food/views/tacos-al-pastor-242132" target="_blank">Al Pastor</a> tacos`
+    }
+  }
+}
+</script>
